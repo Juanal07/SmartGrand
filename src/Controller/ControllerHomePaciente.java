3283f981b4bd688e244d030aec67ceaefdc840ad
juan sebastian quinayas guarin <sebastianquemasda@gmail.com>
2019-11-28 16:58:04 +0100

    responsive login -> sebas

--- src/Controller/ControllerHomePaciente.java
+++ src/Controller/ControllerHomePaciente.java
@@ -1,14 +1,19 @@
 package Controller;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 
 import Model.Tickets;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-public class ControllerHomePaciente {
+public class ControllerHomePaciente implements Initializable {
 	@FXML
 	private JFXListView<Tickets> lvTicketsPaciente = new JFXListView<Tickets>();
 	@FXML
@@ -17,13 +22,24 @@ public class ControllerHomePaciente {
 	private JFXTextArea jfxTaPaciente = new JFXTextArea();
 
 	private ListView<Tickets> listaTickets = new ListView<Tickets>();
-	
 
 	@FXML
-	public void enviarMSM() { 
+	public void enviarMSM() {
 		System.out.println(jfxTaPaciente.getText());
 		jfxTaPaciente.setText("");
-		
-		
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		//lvTicketsPaciente.getItems().addAll(leerTickets());
+
+	}
+
+	public List<Tickets> leerTickets() {
+		List<Tickets> listaTickets = GsonGeneral.desserializarJsonAArrayTicket();
+		return listaTickets;
 	}
 }
