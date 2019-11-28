0e208c3730fa01a45a3b17d7b6b301fbb755653c
Juan Alberto Raya Rodríguez <jjrr1307@gmail.com>
2019-11-28 20:55:18 +0100

    a

--- src/Controller/VistaCuidadorPrincipalController.java
+++ src/Controller/VistaCuidadorPrincipalController.java
@@ -1,14 +1,20 @@
 package Controller;
 
+import java.awt.Button;
+import java.awt.Label;
+
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class VistaCuidadorPrincipalController {
 	
 	 @FXML
-	 private TextField myTextField;
+	 private JFXTextField myTextField;
 	 
-	 //Esta función actualiza la VISTA de la segunda ventana
+	 //Esta funciï¿½n actualiza la VISTA de la segunda ventana
 	 public void writeText(String mensaje) {		
 	     myTextField.setText(mensaje);
 	   }
