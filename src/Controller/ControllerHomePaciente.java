cc3ddd61408604c6f758861791b22b022fa1d15b
sebas <sebas@192.168.1.57>
2019-12-03 11:36:43 +0100

    mostrar tickets en vista paciente

--- src/Controller/ControllerHomePaciente.java
+++ src/Controller/ControllerHomePaciente.java
@@ -1,13 +1,9 @@
 package Controller;
 
 import java.net.URL;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import java.util.List;
-
 import java.util.ResourceBundle;
 
 import com.google.gson.Gson;
@@ -20,16 +16,15 @@ import Model.Medico;
 import Model.Persona;
 import Model.Tickets;
 import Controller.GsonGeneral;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class ControllerHomePaciente {
+public class ControllerHomePaciente implements Initializable {
 	@FXML
-	private JFXListView<Tickets> lvTicketsPaciente = new JFXListView<Tickets>();
+	private JFXListView<String> lvTicketsPaciente = new JFXListView<String>();
 	@FXML
 	private JFXButton fxmBtnEnviarTicket = new JFXButton();
 	@FXML
@@ -37,82 +32,61 @@ public class ControllerHomePaciente {
 	@FXML
 	private Label idPacienteLabel = new Label();
 	@FXML
-	private ObservableList<Tickets> ticketsObservableList;
-	
+	private ObservableList<String> ticketsObservableList;
+
 	public void enviarMSM() {
 		String textoPaciente = jfxTaPaciente.getText();
 		String dniPaciente = idPacienteLabel.getText();
 		String dniMedico = "";
 //		LocalDate date = LocalDate.now();
 //		System.out.println(date);
-		
 		List<Medico> listaMedicoRelacion = GsonGeneral.desserializarJsonAArrayMedico();
-		System.out.println("objetos en medicos.json: " + listaMedicoRelacion.size());
-
 		for (Medico medico : listaMedicoRelacion) {
-			
-			
 			ArrayList<String> idPacientes = medico.getDniPacientes();
-			
-			
-
 			int sizeArray = idPacientes.size();
 			int i = 0;
 			while (i < sizeArray) {
 				if (dniPaciente.equals(idPacientes.get(i))) {
 					System.out.println("dni medico: " + medico.getIdMedico());
 					System.out.println("nº de pacientes del medico: " + idPacientes.size());
-					System.out.println("Dni Paciente Encontrado: " + idPacientes.get(i)+" en la posicion:" + i);
+					System.out.println("Dni Paciente Encontrado: " + idPacientes.get(i) + " en la posicion:" + i);
 					dniMedico = medico.getIdMedico();
 					i = i + sizeArray;
 				}
 				i++;
 			}
 		}
-
 		Tickets nuevo = new Tickets(dniPaciente, dniMedico, textoPaciente, "");
-		System.out.println(dniPaciente + dniMedico + textoPaciente);
-		String ruta = "jsonTickets.json";
-		List<Tickets> tiquets = GsonGeneral.desserializarJsonAArrayTicket(); 
-		System.out.println(tiquets.size());
-		tiquets.add(nuevo);
-		System.out.println(nuevo.getIdClinico());
+		EnviarTicket(nuevo);
+	}
 
+	public void EnviarTicket(Tickets ticket) {
+		String ruta = "jsonTickets.json";
+		List<Tickets> tiquets = GsonGeneral.desserializarJsonAArrayTicket();
+		tiquets.add(ticket);
 		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
 		String representacionBonita = prettyGson.toJson(tiquets);
 		System.out.println(representacionBonita);
 		GsonGeneral.EscribirJson(representacionBonita, ruta);
 	}
 
-//	public void EnviarTicket(Tickets ticket) {
-//		String ruta = "jsonTickets.json";
-//		java.util.List<Tickets> lista = GsonGeneral.desserializarJsonAArrayTicket(); 
-//		lista.add(ticket);
-//		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
-//		String representacionBonita = prettyGson.toJson(lista);
-//		GsonGeneral.EscribirJson(representacionBonita, ruta);
-//	}
-
-//	public List<Tickets> leerTickets(ObservableList<Tickets> ticketsObservableList2) {
-//		List<Tickets> listaTickets = GsonGeneral.desserializarJsonAArrayTicket();
-//		for (Tickets tickets : listaTickets) {
-//			ticketsObservableList2.add(tickets);
-//		}
-//		return ticketsObservableList2;
-//	}
-
 	public void writeText(Persona p) {
-		// TODO Auto-generated method stub
 		idPacienteLabel.setText(p.getDni());
-
 	}
 
-//	@Override
-//	public void initialize(URL location, ResourceBundle resources) {
-//		// TODO Auto-generated method stub
-//		ObservableList<Tickets> ticketsObservableList = FXCollections.observableArrayList();
-//		// ticketsObservableList = (ObservableList<Tickets>)
-//		// leerTickets(ticketsObservableList);
-//		lvTicketsPaciente.setItems(ticketsObservableList);
-//	}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		ObservableList<String> ticketsObservableList = FXCollections.observableArrayList();
+		// ticketsObservableList = (ObservableList<Tickets>)
+		leerTickets(ticketsObservableList);
+		lvTicketsPaciente.setItems(ticketsObservableList);
+	}
+
+	private void leerTickets(ObservableList<String> ticketsObservableList2) {
+		List<Tickets> tiquets = GsonGeneral.desserializarJsonAArrayTicket();
+		for (Tickets tickets : tiquets) {
+			String ticket = tickets.getTextoPaciente() + "\n" + tickets.getTextoClinico(); 
+			ticketsObservableList2.add(ticket);
+		}
+	}
 }
